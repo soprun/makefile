29dfde5c6d137481da721b07ecc267574fcdf2fb
Vladislav Soprun <develop@soprun.com>
2022-01-23 01:28:25 +0300

    Commit...

--- git.inc.mk
+++ git.inc.mk
@@ -1,6 +1,8 @@
 # Git
 # https://www.codegrepper.com/code-examples/shell/git+submodule+update
 
+# https://devconnected.com/how-to-add-and-update-git-submodules/
+
 git-submodule-update: ## to update
 	git pull origin main
 	git submodule update --init
